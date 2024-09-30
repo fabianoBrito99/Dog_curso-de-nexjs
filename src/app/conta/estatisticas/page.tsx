1318f3494c6835ece159b600182e821ff8ca5eb4
Fabiano Brito <fabiano99.fbs@gmail.com>
2024-09-30 17:37:28 -0300

    metaadatas das paginas

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Estatísticas | Minha Conta',
+};
+
 export default async function EstatisticasPage() {
- return <main><h1>Estatistica</h1></main>;
+  return (
+    <main>
+      <h1>Estatistica</h1>
+    </main>
+  );
 }
--- src/app/conta/postar/page.tsx
+++ src/app/conta/postar/page.tsx
@@ -1,3 +1,15 @@
+import { Metadata } from 'next';
+
+export const runtime = 'edge';
+
+export const metadata: Metadata = {
+  title: 'Postar | Minha Conta',
+};
+
 export default async function PostarPage() {
- return <main><h1>Postar</h1></main>;
+  return (
+    <main>
+      <h1>Postar</h1>
+    </main>
+  );
 }
